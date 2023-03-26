6f34286f6ab76977ce123327818fa29681f85613
Mattia Nicastro <nicastromattia@outlook.com>
2023-03-26 13:48:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require("components/head.html") ?>
+<body>
+      <?php require("components/navbar.html") ?>
+      <div class="hero min-h-screen" style="background-image: url(assets/bed-breakfast-820x451.webp);">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+          <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">La Nuova Vela</h1>
+            <p class="mb-5">Prova ad uscirne vivo</p>
+            <button class="btn btn-primary">Prenota ora</button>
+          </div>
+        </div>
+      </div>    
+</body>
+</html>
---
+++ private-area/area-clienti.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require("../components/head.html") ?>
+<body>
+    <?php require("../components/navbar.html") ?>
+    <div class="flex h-screen w-screen bg-primary">
+        <input type="select">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </input>
+    </div>
+
+</body>
+</html>
